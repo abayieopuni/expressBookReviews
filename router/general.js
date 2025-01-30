69077a00303ad32e8f35a8b7f004a6a3f8ec719f
kwame Opuni <abayieremi.com>
2025-01-30 03:14:33 -0500

    adding routes

--- router/general.js
+++ router/general.js
@@ -5,10 +5,28 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+  
+    // Check if username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password required" }); // Changed to 400
+    }
+  
+    // Check if the username is valid
+    if (!isValid(username)) {
+      return res.status(400).json({ message: "Invalid username." }); // Changed to 400
+    }
+  
+    // Check if the username already exists
+    if (users.find(user => user.username === username)) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+  
+    // Register the new user
+    users.push({ username, password });
+    return res.status(200).json({ message: "User successfully registered. Now you can login" });
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -46,14 +64,21 @@ public_users.get('/review/:isbn',function (req, res) {
   const {isbn} = req.params;
   const book = Object.values(books).find(book => book.isbn === isbn);
   
-  if (book && book.reviews){
-    if (Object.keys(book.reviews).length > 0){
-        res.status(200).json(book.reviews)
-    }else{
-        res.status(404).json({message:"No reviews found for this book"})
+  if (book) {
+    if (book.reviews && Object.keys(book.reviews).length > 0) {
+      // Return the reviews if they exist
+      res.status(200).json(book.reviews);
+    } else {
+      // No reviews found for this book
+      res.status(404).json({ message: "No reviews found for this book" });
     }
+  } else {
+    // Book not found
+    res.status(404).json({ message: "Book not found" });
   }
   
 });
+
+  
 
 module.exports.general = public_users;
