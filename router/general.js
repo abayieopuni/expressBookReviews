2a091a379f17f130c00ed4e03d6a3c38002ee25b
abayie opuni <abayieremi@gmail.com>
2025-02-07 17:09:29 -0500

    adding asyc routes

--- router/general.js
+++ router/general.js
@@ -28,34 +28,89 @@ public_users.post("/register", (req, res) => {
     return res.status(200).json({ message: "User successfully registered. Now you can login" });
   });
 
+// Similating asyc function for books
+  async function getBooks () {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(books);
+        }, 1000);
+    });
+  }
+
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',  async (req, res) => {
   //Write your code here
-  res.send(JSON.stringify({books}, null, 4));
-});
+    try {
+        const bookList = await getBooks();
+        res.status(200).send(JSON.stringify({books:bookList}, null , 4))
+    } catch (error) {
+        res.status(500).json({message: "Error fecting books", error: error.message})
+    }
+ 
+})
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async (req, res)  => {
   //Write your code here
    const {isbn} = req.params;
-   const book = Object.values(books).find(book => book.isbn === isbn)
-   res.send(book)
+
+    try {
+        const bookList = await getBooks();
+        const foundBook = Object.values(bookList).find(book => book.isbn === isbn)
+        if (foundBook){
+            res.status(200).json(foundBook)
+        }else {
+            res.status(404).json({message:`Book with isbn ${isbn} not found`})
+        }
+        
+    } catch (error) {
+        res.status(500).json({message: "error fetching books details", error:error.message })
+    }
+
+
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async (req, res)  => {
   //Write your code here
   const {author} = req.params;
-  const bookAuthor = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase())
-  res.status(200).json(bookAuthor)
+
+  try {
+    const bookList = await getBooks();
+    const bookAuthor = Object.values(bookList).filter(book => book.author.toLowerCase() === author.toLowerCase())
+    if (bookAuthor.length > 0){
+        res.status(200).json(bookAuthor)
+    }else {
+        res.status(404).json({message:`No books found by author ${author}`})
+    }
+    
+  } catch (error) {
+    res.status(500).json({message:"Error fectching books by author", error: error.message})
+  }
+
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
   //Write your code here
   const{title} = req.params;
-  const bookTiTle = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase())
-  res.status(200).json(bookTiTle)
+
+  try {
+    const bookList = await getBooks();
+    console.log(bookList)
+
+    const bookTiTle = Object.values(bookList).filter(book => book.title.toLowerCase() === title.toLowerCase())
+    if (bookTiTle.length > 0) {
+        res.status(200).json(bookTiTle)
+    }else {
+        res.status(404).json({message:`No books found with the title ${title}`})
+    }
+   
+  } catch (error) {
+    res.status(500).json({message:"Error fetching books by author", error:error.message})
+  }
+
 });
 
 //  Get book review
